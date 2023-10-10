a0d9cab06b9571a273a345b179c18a0f64687b10
Kokila Abhayasundara <94110759+kvabhaya@users.noreply.github.com>
2023-10-10 15:14:54 +0530

    Create PrintVariables.java

---
+++ Self Codes/PrintVariables.java
@@ -0,0 +1,12 @@
+public class PrintVariables {
+    public static void main(String[] args) {
+        String name = "John";
+        System.out.println("Hello "+name);
+
+        String firstName = "John ";
+        String lastName = "Doe";
+        String fullName = firstName+lastName;
+        System.out.println(fullName);
+    }
+    
+}

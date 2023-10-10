d48a3fd8453ae51654791a7f2901b591f9615166
Kokila Abhayasundara <94110759+kvabhaya@users.noreply.github.com>
2023-10-10 13:13:16 +0530

    Create SampleWindow.java

---
+++ Self Codes/SampleWindow.java
@@ -0,0 +1,12 @@
+/* Sample Program: Displaying a Window
+File: SampleWindow.java */
+import javax.swing.*;
+class SampleWindow {
+    public static void main(String[] args) {
+        JFrame myWindow;
+        myWindow = new JFrame();
+        myWindow.setSize(300, 200);
+        myWindow.setTitle("My Window");
+        myWindow.setVisible(true);
+    }
+}

777e496dc7a326f7654a73e96bc88fb5508be0ec
Kokila Abhayasundara <94110759+kvabhaya@users.noreply.github.com>
2023-10-14 13:10:42 +0530

    Create #6 DataTypes.java

---
+++ Java Tutorial For beginners-Telusko/#6 DataTypes.java
@@ -0,0 +1,10 @@
+public class DataTypes {
+    public static void main(String[] args) 
+        int i = 5;
+        System.out.println(i);
+
+        float myNum;
+        myNum = 20.0;
+        System.out.println(myNum);
+    }
+}

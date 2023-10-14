51ff2e96f468bb481a8f1024e00a8c26d5ee5f2f
Kokila Abhayasundara <94110759+kvabhaya@users.noreply.github.com>
2023-10-14 13:05:34 +0530

    Create #5 variables.java

---
+++ Java Tutorial For beginners-Telusko/#5 variables.java
@@ -0,0 +1,12 @@
+public class Variables {
+    public static void main(String[] args) {
+        int i = 5;
+        System.out.println(i);
+
+        int myNum1 = 20;
+        int myNum2 = 20;
+        int result = myNum1 + myNum2;
+        System.out.println(result);
+
+    }
+}

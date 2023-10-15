4333e15e21bc27166023fbb7b8aa1770bb427119
Kokila Abhayasundara <94110759+kvabhaya@users.noreply.github.com>
2023-10-15 13:51:49 +0530

    Create UserInputAge.java

---
+++ Self Codes/UserInputAge.java
@@ -0,0 +1,10 @@
+import java. util.*;
+public class Main {
+    public static void main(String[] args) {
+      Scanner scanner = new Scanner(System.in);
+      int age;
+      System.out.print("Enter your age: ");
+      age = scanner.nextInt();
+      System.out.println("You are " + age + " years old.");
+    }
+}

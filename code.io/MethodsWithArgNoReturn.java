66e004ab7c0280b69150aea7c8c6b92adae2a12f
Kokila Abhayasundara <94110759+kvabhaya@users.noreply.github.com>
2023-10-15 15:19:00 +0530

    Create MethodsWithArgNoReturn.java

---
+++ code.io/MethodsWithArgNoReturn.java
@@ -0,0 +1,10 @@
+public class MethodsWithArgNoReturn {
+    public static void main(String[] args) {
+        add(10,15);
+        System.out.println("c printed");
+    }
+    public static void add(int a,int b){
+        int c=a+b;
+        System.out.println(c);
+    }
+}

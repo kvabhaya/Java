aba5c20a7860160d456230f28caeddab26449152
Kokila Abhayasundara <94110759+kvabhaya@users.noreply.github.com>
2023-10-15 15:11:01 +0530

    Create Methods.java

---
+++ code.io/Methods.java
@@ -0,0 +1,13 @@
+public class Methods {
+    public static void main(String args[]){
+        add();
+        System.out.println("c printed");
+    }
+
+    public static void add(){
+        int a=10;
+        int b=15;
+        int c=a+b;
+        System.out.println(c);
+    }
+}

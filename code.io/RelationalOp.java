f3ccb821efd31e3ba69aa65bc0fe9c1d6929a5f8
Kokila Abhayasundara <94110759+kvabhaya@users.noreply.github.com>
2023-10-21 21:57:07 +0530

    Create RelationalOp.java

---
+++ code.io/RelationalOp.java
@@ -0,0 +1,11 @@
+public class RelationalOp {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+
+        System.out.println(a>b);
+        System.out.println(a<b);
+        System.out.println(a==b);
+        System.out.println(a!=b);
+    }
+}

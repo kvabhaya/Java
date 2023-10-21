b9e7617ce7e8a4a6d8f87d1761360488f0654f70
Kokila Abhayasundara <94110759+kvabhaya@users.noreply.github.com>
2023-10-21 22:00:56 +0530

    Create LogicalOp.java

---
+++ code.io/LogicalOp.java
@@ -0,0 +1,13 @@
+public class LogicalOp {
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+
+        System.out.println(!a);
+        System.out.println(!b);
+
+        System.out.println(a||b);
+
+        System.out.println(a&&b);
+    }
+}

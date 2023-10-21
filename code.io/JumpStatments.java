801b1e6b253b475732b42770a56ff2307ad5abad
Kokila Abhayasundara <94110759+kvabhaya@users.noreply.github.com>
2023-10-21 23:10:50 +0530

    Create JumpStatments.java

---
+++ code.io/JumpStatments.java
@@ -0,0 +1,13 @@
+public class JumpStatments {
+    public static void main(String[] args) {
+        for (int i = 1;i<=10;i++) {
+            if (i == 3) {
+                continue;
+            }
+            else if (i == 7) {
+                break;
+            }
+            System.out.println(i);
+        }
+    }
+}

7fda79f550d2f56bb2093f092992d9e5305f40bb
Kokila Abhayasundara <94110759+kvabhaya@users.noreply.github.com>
2023-10-21 21:15:30 +0530

    Create ArithmeticOp.java

---
+++ code.io/ArithmeticOp.java
@@ -0,0 +1,19 @@
+public class ArithmeticOp {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+
+        int c = a+b;
+        int d = a-b;
+        int e = a*b;
+        float f = (float) a/ (float) b;
+        int g = a%b;
+
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+
+    }
+}

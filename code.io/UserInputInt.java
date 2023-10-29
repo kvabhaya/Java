623c5e8800a69ce6741f9b32c3ded69e0d1ad719
Kokila Abhayasundara <94110759+kvabhaya@users.noreply.github.com>
2023-10-29 16:18:50 +0530

    Create UserInputInt.java

---
+++ code.io/UserInputInt.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class UserInputInt {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+
+        System.out.println(a);
+    }
+}

5ccd42278e1db9ff180dac5e8725e84c0b79542c
Kokila Abhayasundara <94110759+kvabhaya@users.noreply.github.com>
2023-10-29 16:23:02 +0530

    Create UserInputChar.java

---
+++ code.io/UserInputChar.java
@@ -0,0 +1,8 @@
+import java. util.*;
+public class UserInputChar {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        char c = input.next().charAt(0);
+        System.out.println(c);
+    }
+}

f68da626e1cb4a8633247fab9f37f49f70894a3c
Kokila Abhayasundara <94110759+kvabhaya@users.noreply.github.com>
2023-10-29 17:47:55 +0530

    Create Summation.java

---
+++ Practice Coding/Methods and Function/Summation.java
@@ -0,0 +1,15 @@
+//Write a Java method that accepts two integers and returns their sum.
+
+
+public class Summation {
+
+    public static int sum(int num1,int num2) {
+        return num1+num2;
+    }
+
+    public static void main(String[] args) {
+        int x = sum(6,7);
+        System.out.println(x);
+    }
+}
+

8eafdf2e631572f1c04f2fa4df26e5dbf4d435c2
Kokila Abhayasundara <94110759+kvabhaya@users.noreply.github.com>
2023-12-11 09:36:39 +0530

    Add files via upload

---
+++ code.io/Packages/Demo.java
@@ -0,0 +1,9 @@
+import tools.AdvCalc;
+import tools.Calc;
+
+public class Demo {
+    public static void main(String[] args) {
+        Calc obj = new Calc();
+        AdvCalc obj1 = new AdvCalc(); 
+    }
+}
